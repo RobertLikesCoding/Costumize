4d5f319746a574c61c34c85f53c39ad9cbb9639a
Robert Baufeld <rbaufeld@posteo.de>
2024-08-10 12:29:44 +0200

    added basic validation model test for costumes

--- test/models/costume_test.rb
+++ test/models/costume_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class CostumeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Costume should not be saved without a name" do
+    costume = Costume.new
+    assert_not costume.save, "Saved the costume without a name"
+    costume.valid?
+    puts costume.errors.full_messages
+  end
 end
